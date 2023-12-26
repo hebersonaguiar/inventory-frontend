8cbe8350150df614734f36c67ec2d0e9b136702c
hebersonaguiar <hebersonaguiar@gmail.com>
2023-12-25 21:55:28 -0300

    Add edit button

--- public/js/functions.js
+++ public/js/functions.js
@@ -77,7 +77,7 @@ function parseHostname() {
 
 
 $(document).ready(function() {
-  var hostEdit = document.getElementById("edit-hostName");
+  var hostEdit = document.getElementById("edit-hostName").value;
   console.log(hostEdit);
 
   const elHostname = document.getElementById('in-hostname');
