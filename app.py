513e2a0f4aa60a317305d5e4aaac41d7966e0b65
hebersonaguiar <hebersonaguiar@gmail.com>
2024-02-07 21:57:40 -0300

    DEAL WITH IT

---
+++ app.py
@@ -0,0 +1,49 @@
+import json, re, csv
+from flask import Flask, render_template, request, requests, redirect, url_for, flash, stream_with_context, g, session
+from flask_restful import Resource, Api
+from flask_mysqldb import MySQL
+from flask_cors import CORS
+from flask_jsonpify import jsonify
+from json import dumps
+from datetime import datetime
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.secret_key = "flash message"
+
+
+@app.route('/servers')
+def servers():
+	# if g.username:
+    try:
+
+        api_url = "http://inventory:5000/hosts/"
+        response = requests.get(api_url)
+        print(response.json())
+
+        # cur = mysql.connection.cursor()
+        # cur.execute("SELECT * FROM users")
+        # data = cur.fetchall()
+        # cur.close()
+
+        # user = g.username
+
+        # domain_name = 'mdh.gov.br'
+        # domain      = domain_name.split('.')
+        # connect     = conn()
+
+        # connect.search('dc={},dc={},dc={}'.format(domain[0], domain[1], domain[2]), '(sAMAccountName={})'.format(user), attributes = [ 'sAMAccountName', 'distinguishedName', 'displayName'], search_scope=SUBTREE )
+
+        # displayNameObj  = connect.entries[0].displayName.value
+        # displayName = str(displayNameObj)
+
+        return render_template('servers.html', users=data)
+
+    except Exception as e:
+        return redirect(url_for('servers'))
+
+	# return redirect(url_for('servers'))
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0', port='3000')
