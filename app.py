42b5a37568987f096fe0649e81269db166117dd1
hebersonaguiar <hebersonaguiar@gmail.com>
2024-02-07 22:18:01 -0300

    more ignored words

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json, re, csv, requests
 from flask import Flask, render_template, request, redirect, url_for, flash, stream_with_context, g, session
 from flask_restful import Resource, Api
-from flask_mysqldb import MySQL
+# from flask_mysqldb import MySQL
 from flask_cors import CORS
 from flask_jsonpify import jsonify
 from json import dumps
