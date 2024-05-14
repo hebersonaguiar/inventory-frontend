4f7fbfcced5e8b94f8b12be6b5e0cc8087dc6f31
hebersonaguiar <hebersonaguiar@gmail.com>
2024-05-14 20:55:36 -0300

    feat: adjustment html

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ def index():
 
 @app.route('/servers', methods=['GET'])
 def servers():
-	# if g.username:
     try:
 
         api_url = "http://inventory:5000/hosts"
@@ -38,8 +37,27 @@ def servers():
         # return redirect(url_for('servers'))
         print(e)
         return redirect(url_for('index'))
+    
+@app.route('/edit/<string:servername', methods=['GET'])
+def edit(servername):
+    try:
 
-	# return redirect(url_for('servers'))
+        api_url = "http://inventory:5000/hosts/{}".format(servername)
+        response = requests.get(api_url)
+
+        if response.status_code == 200:
+             data = json.loads(response.text)
+            #  print(data) #pint json list
+        else:
+             print(f"Error retrieving data, status code: {response.status_code}")
+
+        return render_template('edit.html', edit=data)
+        # return response.json()
+
+    except Exception as e:
+        # return redirect(url_for('servers'))
+        print(e)
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port='5000')
