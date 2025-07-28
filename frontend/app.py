7d6e1206c311b04d020b530201b3bcb381e77312
Heberson Aguiar <hebersonaguiar@gmail.com>
2025-07-27 21:23:12 -0300

    Change login to use api with database user stored

--- frontend/app.py
+++ frontend/app.py
@@ -22,15 +22,37 @@ def login_page():
 
     username = st.text_input("Usuário")
     password = st.text_input("Senha", type="password")
+    login_button = st.button("Entrar")
 
-    if st.button("Entrar"):
-        if check_login(username, password):
-            st.session_state.logged_in = True
-            st.session_state.username = username
-            st.success("Login realizado com sucesso!")
-            st.rerun()
-        else:
-            st.error("Usuário ou senha inválidos!")
+    if login_button:
+        try:
+            response = requests.post(f"{API_URL}/api/v1/auth/login", json={
+                "username": username,
+                "password": password
+            })
+
+            if response.status_code in (200, 201):
+                token = response.json.get("access_token")
+                st.session_state["jwt_token"] = token
+                st.session_state["logged_in"] = True
+                st.success("Login realizado com sucesso!")
+                st.rerun()
+            else:
+                st.error("Usuário ou senha inválidos!")
+        except Exception as e:
+                st.error(f"Erro ao tentar login: {e}")
+
+
+
+
+    # if st.button("Entrar"):
+    #     if check_login(username, password):
+    #         st.session_state.logged_in = True
+    #         st.session_state.username = username
+    #         st.success("Login realizado com sucesso!")
+    #         st.rerun()
+    #     else:
+    #         st.error("Usuário ou senha inválidos!")
 
 def main_app():
 
